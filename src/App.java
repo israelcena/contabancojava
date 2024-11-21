b3a614962725d86476813ee4696167adf9305538
Israel Cena <israelsjm@gmail.com>
2024-11-20 22:27:11 -0300

    include data

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        ContaTerminal contaTerminal = new ContaTerminal();
+
+        System.out.println("Por favor, digite o número da Agência (com digito)!");
+        contaTerminal.setAgencia(scanner.next());
+        System.out.println("Agora digite o número da Conta (somente números)");
+        contaTerminal.setNumero(scanner.nextInt());
+        System.out.println("Agora digite seu nome completo");
+        contaTerminal.setNomeCliente(scanner.next());
+        scanner.nextLine(); 
+        System.out.println("Agora digite o valor da conta");
+        contaTerminal.setSaldo(scanner.nextDouble());
+
+        String mensagem = "Olá "
+        .concat(contaTerminal.getNomeCliente())
+        .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+        .concat(contaTerminal.getAgencia())
+        .concat(", conta ")
+        .concat(String.valueOf(contaTerminal.getNumero()))
+        .concat(" e seu saldo ")
+        .concat(String.valueOf(contaTerminal.getSaldo()))
+        .concat(" já está disponível para saque.");
+
+        System.out.println(mensagem);
+        
+        scanner.close();
     }
 }
