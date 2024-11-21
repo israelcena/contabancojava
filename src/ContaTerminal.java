be93169ffe3cae94d0ebca14fe679c3fc4494734
Israel Cena <israelsjm@gmail.com>
2024-11-20 22:26:51 -0300

    create ContaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,31 @@
+public class ContaTerminal {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public String getAgencia() {
+        return agencia;
+    }
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
